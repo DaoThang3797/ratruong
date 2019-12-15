08368b95e01f4b16ae49c8958ca6024577f1a432
Dao Van Thang (Odoo) <thangdv@magenest.com>
2019-12-15 23:18:26 +0700

    C:/Program Files/Git/

---
+++ Dump20191215.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ratruong
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `bantochuc`
+--
+
+LOCK TABLES `bantochuc` WRITE;
+/*!40000 ALTER TABLE `bantochuc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bantochuc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `cauthu`
+--
+
+LOCK TABLES `cauthu` WRITE;
+/*!40000 ALTER TABLE `cauthu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cauthu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `daitruyenhinh`
+--
+
+LOCK TABLES `daitruyenhinh` WRITE;
+/*!40000 ALTER TABLE `daitruyenhinh` DISABLE KEYS */;
+/*!40000 ALTER TABLE `daitruyenhinh` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `doibong`
+--
+
+LOCK TABLES `doibong` WRITE;
+/*!40000 ALTER TABLE `doibong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doibong` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nguoi`
+--
+
+LOCK TABLES `nguoi` WRITE;
+/*!40000 ALTER TABLE `nguoi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nguoi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `quanly`
+--
+
+LOCK TABLES `quanly` WRITE;
+/*!40000 ALTER TABLE `quanly` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quanly` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `sanbong`
+--
+
+LOCK TABLES `sanbong` WRITE;
+/*!40000 ALTER TABLE `sanbong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sanbong` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `taikhoan`
+--
+
+LOCK TABLES `taikhoan` WRITE;
+/*!40000 ALTER TABLE `taikhoan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `taikhoan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `thongtin`
+--
+
+LOCK TABLES `thongtin` WRITE;
+/*!40000 ALTER TABLE `thongtin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `thongtin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `trandau`
+--
+
+LOCK TABLES `trandau` WRITE;
+/*!40000 ALTER TABLE `trandau` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trandau` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `trongtai`
+--
+
+LOCK TABLES `trongtai` WRITE;
+/*!40000 ALTER TABLE `trongtai` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trongtai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `vongdau`
+--
+
+LOCK TABLES `vongdau` WRITE;
+/*!40000 ALTER TABLE `vongdau` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vongdau` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-15 23:17:53
